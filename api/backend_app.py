f6414a5e63ce728d87baab4768867a8437542135
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2025-03-29 10:19:48 -0400

    More Notes from Class

--- api/backend_app.py
+++ api/backend_app.py
@@ -17,3 +17,6 @@ if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0', port = 4000)
     #This starts the flask container 
     #Youre running it in debug mode so you get more specific error messages
+    #The 0.0.0.0 means it accepts from all IP addresses 
+    #The 4000 is what port it is running on - the testing file there a line that redirects it to port 
+    #4001 because you cant have the same port for your testing and your group testing 
